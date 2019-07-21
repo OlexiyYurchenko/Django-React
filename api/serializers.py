a993f1a78279edc1c6f983a385f62e1be0bebce7
Alexey Yurchenko <olexiy.yurchenko@gmail.com>
2019-07-21 20:01:40 +0300

    test

--- api/serializers.py
+++ api/serializers.py
@@ -6,6 +6,7 @@ from first_project.models import *
 class ArticlePreviewSerializer(serializers.ModelSerializer):
 
     autor_name = serializers.ReadOnlyField(source='autor.username')
+    created_at = serializers.DateTimeField(format="%Y.%m.%d %H:%M")
     class Meta:
         model = Article
         fields = [
