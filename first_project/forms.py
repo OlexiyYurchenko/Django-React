ebfa8174e5a41463c3f0b441bad240844acfd1c3
Alexey Yurchenko <olexiy.yurchenko@gmail.com>
2019-08-16 05:58:36 +0300

    2

--- first_project/forms.py
+++ first_project/forms.py
@@ -18,5 +18,5 @@ class ProfileForm(forms.ModelForm):
 
     class Meta:
         model = Profile
-        fields = ('date_of_birth', 'photo')
+        fields = ('date_of_birth', 'image')
         
--- first_project/models.py
+++ first_project/models.py
@@ -74,7 +74,6 @@ class Comment(models.Model):
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile', null=True)
     date_of_birth = models.DateField(blank=True, null=True)
-    photo = models.ImageField(upload_to='users/%Y/%m/%d', blank=True)
     image = CloudinaryField('image', null=True, blank=True)
 
     def __str__(self):
