775742b82933643de06b62217aedc4902ebcc9cf
Alexey Yurchenko <olexiy.yurchenko@gmail.com>
2019-08-16 20:52:19 +0300

    5

--- source/settings.py
+++ source/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'h+sw)ge!pd^sbt$!&yqq*96qq0e6!(ms2_c0nxo2!v#oqb6!*$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
